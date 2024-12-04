fbdf4f20e85185b304de6557d6bb5e9fbd97e80e
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-04 15:22:49 +0500

    1

--- 18-6-anketa/src/App.vue
+++ 18-6-anketa/src/App.vue
@@ -1,14 +1,61 @@
-
 <template>
- 
+  <div class="container">
+    <form class="card" @submit.prevent="submitHandler">
+      <h1>Анкета на VUE разработчика!</h1>
+      <div class="form-control">
+        <label for="name">Как тебя зовот</label>
+        <input type="text" id="name" placeholder="Введите имя" />
+      </div>
+
+      <div class="form-control">
+        <label for="age">Выбери возраст</label>
+        <input type="number" id="age" value="20" />
+      </div>
+
+      <div class="form-control">
+        <label for="city">Твой город</label>
+        <select id="sity">
+          <option value="spb">Санкт-Петербург</option>
+          <option value="msk">Москва</option>
+          <option value="kzn">Казань</option>
+          <option selected value="nsk">Новосибирск</option>
+        </select>
+      </div>
+
+      <div class="form-checkbox">
+        <span>Готовы к переезду в Токио?</span>
+        <div class="checkbox">
+          <label><input type="radio" name="trip" />Да</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="radio" name="trip" />Нет</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Что знаеш о Vue?</span>
+        <div class="checkbox">
+          <label><input type="checkbox" />Vuex</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" />Vue CLI</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" />Vuex Router</label>
+        </div>
+      </div>
+
+      <button type="submit" class="btn-primary">Отправить</button>
+    </form>
+  </div>
 </template>
 
-
-<script >
-
+<script>
+export default {
+  methods: {
+    submitHandler() {},
+  },
+};
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
